0f0e8fb605f08591397f4605dcff2da7e85661c2
aqsa432 <144125319+aqsa432@users.noreply.github.com>
2023-09-23 16:32:54 -0700

    Add files via upload

---
+++ 01_Variables.js
@@ -0,0 +1,12 @@
+const accountId=144553
+let accountEmail="apervaiz123@google.com"
+var accountPasssword="12345"
+accountCity="JAIPUR"
+
+// accountId=2 // not allowed
+
+accountEmail="hc@hc.com"
+accountPasssword="bhnj"
+accountCity="london"
+
+console.log(accountId);
---
+++ script.js
@@ -0,0 +1,13 @@
+console.log(AQSA)
+const accountId=144553
+let accountEmail="apervaiz123@google.com"
+var accountPasssword="12345"
+accountCity="JAIPUR"
+
+// accountId=2 // not allowed
+
+accountEmail="hc@hc.com"
+accountPasssword="bhnj"
+accountCity="london"
+
+console.log(accountId);
