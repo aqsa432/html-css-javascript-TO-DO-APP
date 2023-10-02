0a9b7c13aaddc93b964962b725e1b1379d1c5b12
aqsa432 <144125319+aqsa432@users.noreply.github.com>
2023-10-02 10:49:25 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,45 @@
-console.log(AQSA)
-const accountId=144553
-let accountEmail="apervaiz123@google.com"
-var accountPasssword="12345"
-accountCity="JAIPUR"
-
-// accountId=2 // not allowed
-
-accountEmail="hc@hc.com"
-accountPasssword="bhnj"
-accountCity="london"
-
-console.log(accountId);
+let input = document.querySelector("input");
+let list = document.querySelector(".tasks-list");
+let task = document.querySelector(".clear-task")
+let alert = document.querySelector("#alertBox")
+function addToDo() {
+    if (input.value !== "") {
+        let newTask = document.createElement("div");
+        newTask.classList.add("d-flex")
+        newTask.classList.add("justify-content-center")
+        newTask.classList.add("my-3")
+        newTask.innerHTML = `<li><p style = "width : 500px; display: inline-block">${input.value}</p><button class = "btn btn-success" onClick = editToDo(this)><i class="fa fa-pencil-square-o"></i></button> <button class = "clear-task btn btn-danger" onclick = clearThisToDo(this)><i class="fa fa-trash-o"></i></button></li>`;
+        list.appendChild(newTask);
+        input.value = ""
+        alertBox.style.display = "none";
+    }
+    else {
+        alertBox.style.display = "block";
+    }
+}
+function clearThisToDo(i) {
+    console.log(i.parentNode.remove())
+}
+function clearAll() {
+    list.innerHTML = ""
+    input.value = ""
+    alertBox.style.display = "none";
+}
+function editToDo(item) {
+    if (item.innerHTML === "Done") {
+        const toDoName1 = item.previousElementSibling.value;
+        const par = document.createElement("p")
+        par.setAttribute("style", "display: inline-block; width : 500px")
+        par.innerHTML = toDoName1;
+        item.parentElement.replaceChild(par, item.previousElementSibling)
+        item.innerHTML = `<i class="fa fa-pencil-square-o"></i>`
+    } else {
+        const toDoName = item.previousElementSibling.textContent;
+        item.textContent = "Done";
+        const input = document.createElement("input");
+        input.value = toDoName;
+        input.type = "text";
+        input.classList.add("mx-5")
+        item.parentElement.replaceChild(input, item.previousElementSibling)
+    }
+}
